9a4ddb27a36e11c71b2709020eb1365977be4d13
Mohammad Danish <81629353+itz-danish@users.noreply.github.com>
2024-03-26 02:27:33 +0530

    Instagram DM bot using selenium

--- README.md
+++ README.md
@@ -1,23 +1,76 @@
 
-Instagram Direct Message Automation
+# Instagram Direct Message Automation Using Selenium
 
-This Python script automates the process of sending direct messages (DMs) on Instagram using the Selenium WebDriver. The script targets specific Instagram profiles and sends predefined messages to them.
+### This Python script automates the process of sending direct messages (DMs) on Instagram using the Selenium WebDriver. The script targets specific Instagram profiles and sends predefined messages to them.
 
-Features:
+## Features:
 
--> Automated Login: The script automates the login process to Instagram using provided credentials.
--> Targeted DMs: It allows specifying a list of target Instagram profiles to send DMs to.
--> Profile Filtering: The script filters out profiles based on a minimum followers limit, ensuring that DMs are sent only to profiles meeting certain criteria.
--> Message Sending: It sends predefined messages to the targeted profiles.
--> Error Handling: The script includes error handling to deal with exceptions that may occur during the automation process.
+- <p>Automated Login: The script automates the login process to Instagram using provided credentials.</p>
+- <p>Targeted DMs: It allows specifying a list of target Instagram profiles to send DMs to.</p>
+- <p>Profile Filtering: The script filters out profiles based on a minimum followers limit, ensuring that DMs are sent only to profiles meeting certain criteria.</p>
+- <p>Message Sending: It sends predefined messages to the targeted profiles.</p>
+- <p>Error Handling: The script includes error handling to deal with exceptions that may occur during the automation process.</p>
 
-Usage:
+## Usage:
 
-Set up the script by providing your Instagram username, password, and other parameters such as the number of DMs to send and the minimum followers limit.
+```
+
+#Path of your chrome web driver
+chrome_webDriver_path = "path of your chrome webdriver"
+#eg.- "C:\\Users\\danish\\Downloads\\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
+min_followers_limit = 125
+loginUsername = "yourUsername"
+loginPassword = "yourPassword"
+no_of_dm_to_send = 20
+```
+
+Set up the script by providing your <strong>Instagram username</strong>, <strong>password</strong>, and other parameters such as the <strong>number of DMs to send</strong>, <strong>path of your chrome webdriver</strong> and the <strong>minimum followers limit</strong>.
+
+- Copy the message that you want to send. It'll paste the message you copied and send it.
+  
 Run the script, and it will automate the process of logging in, searching for target profiles, sending DMs, and handling errors.
-Dependencies:
 
--> Selenium: A Python library used for automating web browser interaction.
--> Chrome WebDriver: The script uses the Chrome WebDriver for browser automation.
+## Dependencies:
 
-Note: Before running the script, ensure you have installed the necessary dependencies and have the Chrome WebDriver installed and configured.
+- <strong>Selenium:</strong> A Python library used for automating web browser interaction.
+- <strong>Chrome WebDriver:</strong> The script uses the Chrome WebDriver for browser automation.
+
+<strong>Note:</strong> Before running the script, ensure you have installed the necessary dependencies and have the Chrome WebDriver installed and configured.
+
+## Working
+1. **Overview:**
+   1.1 The provided script automates interactions with Instagram using the Selenium library. It performs tasks such as logging in, searching for profiles, following accounts, and sending direct messages.
+
+2. **Setup and Configuration:**
+   2.1 Importing Libraries: The script imports necessary libraries including `webdriver` from Selenium, and various modules and functions.
+   2.2 Configuration Variables: Configuration variables like `chrome_webDriver_path`,  `min_followers_limit`, `loginUsername`, `loginPassword`, and `no_of_dm_to_send` are set to define parameters such as minimum follower count, login credentials, and the number of direct messages to send.
+
+3. **WebDriver Initialization:**
+   3.1 The script sets up a WebDriver for Chrome with custom options such as disabling images and plugins.
+
+4. **Functions Definition:**
+   4.1 Several functions are defined to encapsulate specific tasks like logging in, searching for profiles, following accounts, sending direct messages, etc.
+
+5. **Main Flow:**
+   5.1 The main logic is implemented in the `main_flow()` function. It iterates over a list of target Instagram accounts (`target_lst`). For each target account:
+       5.1.1 It searches for the profile.
+       5.1.2 Checks if the account has a sufficient number of followers (notHighProfile function).
+       5.1.3 Attempts to follow the account if it hasn't been followed already.
+       5.1.4 Navigates to the message section of the profile.
+       5.1.5 Types and sends a predefined message to the account.
+   
+   5.2 The loop continues until the specified number of direct messages (`no_of_dm_to_send`) have been sent or until there are no more target accounts left.
+
+6. **Execution:**
+   6.1 The script executes the `main_flow()` function, triggering the automation process. Progress messages are printed indicating the number of direct messages sent, low-profile accounts skipped, and total targets hit.
+
+7. **Conclusion:**
+   7.1 Once the desired number of direct messages is sent or all target accounts are processed, the script terminates with an appropriate message.
+
+### Made with ❤️ by <strong>MOHAMMAD DANISH</strong>
+
+[Instagram](https://instagram.com/_itz_danish_ "Author's Instagram")
+
+[Github](https://github.com/itz-danish "Author's Github")
+
+[Linked in](https://www.linkedin.com/in/mohammad-danish-76570a24a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "Author's Linkedin")
---
+++ bot.py
@@ -0,0 +1,209 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+import time
+import random
+
+#Path of your chrome web driver
+chrome_webDriver_path = "path of your chrome webdriver"
+#eg.- "C:\\Users\\danish\\Downloads\\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
+min_followers_limit = 125
+loginUsername = "yourUsername"
+loginPassword = "yourPassword"
+no_of_dm_to_send = 20
+
+
+
+
+# Create Chrome WebDriver with custom options
+chrome_options = Options()
+chrome_prefs = {"profile.default_content_setting_values": {"images": 1, "plugins": 2}}
+chrome_options.add_experimental_option("prefs", chrome_prefs)
+s = Service(chrome_webDriver_path)
+
+
+driver = webdriver.Chrome(service = s, options=chrome_options)
+no_of_dm_send = 0
+no_of_low_profile_ids = 0
+total_no_of_targets_hit = 0
+driver.get("https://www.instagram.com/")
+
+#list of usernames to send DM
+target_lst = ['bts_x_blackpink_fan.47', 's_radha_krishna_premik', 'sumaira_14_khan']
+
+target_lst = list(set(target_lst))
+
+def get_delay():
+    n = random.randint(5,7)
+    time.sleep(n)
+
+def get_small_delay():
+    n = random.randint(2,3)
+    time.sleep(n)
+
+def log_in(loginUsername,loginPassword):
+    print("Initializing log in...")
+    get_delay()
+    #getting username field
+    username = driver.find_element_by_xpath("""/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input""")
+    username.send_keys(loginUsername)
+
+    #getting password field
+    password = driver.find_element_by_xpath("""/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input""")
+    password.send_keys(loginPassword)
+
+    #clicking log in button
+    driver.find_element_by_xpath("""/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div""").click()
+    get_delay()
+    
+    print("Logged in succesfully...")
+    get_small_delay()
+
+    try:
+        #Trying to click on Save account's "Not now" button to not save the password
+        driver.find_element_by_xpath("""//*[contains(text(),'Not now')]""").click()
+        time.sleep(2)
+    except NoSuchElementException:
+        ij=0
+    time.sleep(4)
+    try:
+        #Trying to click on "Not now" button on the "Turn on notifications" dialog
+        driver.find_element_by_xpath("""//*[contains(text(),'Not Now')]""").click()
+
+    except NoSuchElementException:
+        ij=0
+
+def search_profile(acc_username):
+
+    username_link = "https://www.instagram.com/"
+    username_link += acc_username
+
+    #going to profile
+    driver.get(username_link)
+
+def follow():
+    #Clicking on follow button
+    driver.find_element_by_xpath("""//*[contains(text(),'Follow')]""").click()
+
+def goTo_msg():
+    #clicking on messege
+    driver.find_element_by_xpath("""//div[contains(text(),'Message')]""").click()
+    
+def type_msg():
+    #Getting message box and Clicking on it
+    message_sec = driver.find_element_by_xpath("""/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[1]/p""")
+    message_sec.click()
+    #Pasting the coppied text to message
+    message_sec.send_keys(Keys.CONTROL, 'v')
+
+def send_msg():
+    #clicking send message
+    driver.find_element_by_xpath("""/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[3]""").click()
+
+def notHighProfile(target):
+    #Checking if the target account has required no. of followers or not
+    try:
+        followersCount = int(driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a/span").get_attribute("title").replace(",",""))
+
+        if followersCount > min_followers_limit:
+            #print("Yes a high profile: ",target)
+            return False
+        else:
+            #print("Not a high profile: ",target)
+            return True
+    #Excepting all errors
+    except NoSuchElementException:
+        #print("Not a high profile: ",target)
+        return True
+    ##xcepting value errors that occurs
+    except ValueError:
+        #print("Not a high profile: ",target)
+        return True
+
+
+#Main flow
+log_in(loginUsername,loginPassword)
+get_delay()
+#Running a loop to get target accounts
+for target in target_lst:
+    
+    total_no_of_targets_hit += 1
+    print("\n\n\n\n\n\n-> No. of DMs sent: ",no_of_dm_send)
+    print("-> No. of low profile users: ",no_of_low_profile_ids)
+    print("-> Total no. of target hitted: ",total_no_of_targets_hit)
+
+    #Searching the profile
+    search_profile(target)
+    print("Searching profile: ", target)
+    get_delay()
+    #Removing the target's username from target list
+    target_lst.remove(target)
+    #Checking the account's followers count 
+    if notHighProfile(target):
+        print("Acoount skipped...")
+        no_of_low_profile_ids +=  1
+        continue
+    #Trying to follow
+    try:
+        time.sleep(3)
+        follow()
+        print("Trying to follow")
+    
+    except NoSuchElementException:
+        print("Account already followed... Skipping to next profile...")
+        continue
+    
+    get_delay()
+
+    #Trying to go to the message section
+    try:
+        get_small_delay()
+        goTo_msg()
+        print("Trying to go to msg section...")
+    
+    except NoSuchElementException:
+        print("Unable to go to msg... Skipping to next profle...")
+        continue
+    except ElementClickInterceptedException:
+        print("Unable to go to msg... Skipping to next profle...")
+        continue
+    except Exception as e:
+        print("Unable to go to msg... Skipping to next profle...")
+        continue
+    
+    get_delay()
+
+    #Trying to type the message
+    try:
+        time.sleep(1)
+        type_msg()
+        print("Typing the msg...")
+        time.sleep(1)
+    except NoSuchElementException:
+        print("Message not allowed... Skipping to next profile...")
+        continue
+    
+    #Trying to send the message
+    try:
+        time.sleep(1)
+        send_msg()
+        print("Sending the msg...")
+    except NoSuchElementException:
+        print("Unable to send the msg... Skipping to next profile")
+        continue
+    except ElementClickInterceptedException:
+        print("Unable to send the msg... Skipping to next profile")
+        continue
+    no_of_dm_send += 1    
+    
+    get_small_delay()
+
+    #Chcking if the a desired no. of dm are send
+    if no_of_dm_send == no_of_dm_to_send:
+        print(target_lst)
+        print("Take some rest... 20 DMs sent succesfully!!!")
+        break
+
+print("\n\n\n\n\n\nAccount list empty!!!")
